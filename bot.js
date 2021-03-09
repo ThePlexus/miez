6e831cdea7ba74eb2d6c6d6d9e100c666228e080
ThePlexus <39408844+ThePlexus@users.noreply.github.com>
2021-03-09 13:38:46 +0100

    add welcome message

--- bot.js
+++ bot.js
@@ -1,10 +1,23 @@
-const { Client } = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 const client = new Client();
 const config = require("./data/config.json");
 
 client.on("ready", function () {
     console.log("Hello world!");
     console.log("I am " + client.user.tag);
+});
+
+client.on("guildMemberAdd", function (member) {
+    member
+        .send(
+            new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle("Welcome!")
+                .setDescription("some text, bla bla, extra bla")
+                .setFooter("Send by Miez")
+                .setTimestamp()
+        )
+        .catch();
 });
 
 client.on("message", function (message) {
